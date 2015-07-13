d9008d7fec0a2eb79397d8ec3e16b707302dcce4
afodor88 <afodor88@gmail.com>
2015-07-14 01:14:04 +0300

    teste

--- ProiectPython/Curs/Curs2/test.py
+++ ProiectPython/Curs/Curs2/test.py
@@ -1,5 +1,8 @@
-for var_t in dictionar.values():
+"""for var_t in dictionar.values():
     if (var_t.find("As")!=-1):
-    print var_t
-    
-    
+    print var_t"""
+x =""
+if x:
+    print "True"
+else:
+    print "False"
